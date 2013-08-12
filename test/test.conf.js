f278682965f16e327af4e3fa4ceafef0ec22548a
Oliver Joseph Ash <oliverash@me.com>
2013-08-12 14:21:24 +0100

    Update for new Bower config

--- test/test.conf.js
+++ test/test.conf.js
@@ -2,9 +2,9 @@ basePath = '..';
 files = [
   JASMINE,
   JASMINE_ADAPTER,
-  'components/angular/angular.js',
-  'components/angular-mocks/angular-mocks.js',
-  'components/tinymce-release/tiny_mce.js',
+  'bower_components/angular/angular.js',
+  'bower_components/angular-mocks/angular-mocks.js',
+  'bower_components/tinymce-release/tiny_mce.js',
   'src/tinymce.js',
   'test/*.spec.js'
 ];
